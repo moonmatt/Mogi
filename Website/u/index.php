abd26d63bfdf561d79d9f4a4d88de96b21404eff
MatteoGalavotti <pisucca11@gmail.com>
2019-08-11 19:40:25 +0200

    Added stuff

--- Website/u/index.php
+++ Website/u/index.php
@@ -3,6 +3,7 @@
     $num1 = rand(1,10);
     $num2 = rand(3,15);
     $result = $num1 + $num2;
+    $btime = "25/08/2019 at 3pm";
     $bname = "Early Supporter";
     $blink = "https://i.ibb.co/545y0vF/icon.jpg";
     $maxbadges = 5;
@@ -15,9 +16,7 @@ if (isset($_POST['submit'])) {
     $number = $_POST['number'];
     if ($number = $result) {
         if ($badgenum < $b){
-        $a = ++$badgenum;
-        file_put_contents($file, $a);
-        fclose($myfile);
+        $time = date("d-m-Y");
         $email = $_POST['email'];
         $name = $_POST['name'];
         $ulink = "https://mogi.moonmatt.dev/u/" . $name;
@@ -40,8 +39,73 @@ if (isset($_POST['submit'])) {
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $rec = $email;
-        mail($rec,$sub,$msg,$headers);
-        header('location: yay.php');
+        $fname = $name . ".php";
+        $data = '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title>Mogi Framework - fast and free</title>
+        <link id="pagestyle" rel="stylesheet" type="text/css" href="..\css\mogi.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
+        <script src="../js/style.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <title>'. $name. ' - Mogi Badges</title>
+        </head>
+        <body>
+        <div class="header clearfix sticky">
+        <div class="header__content boxed">
+                <div class="header__logo__img" style="margin: 10px;">
+            <img src="../img/logo.png" alt="" width="40px" height="40px;">
+        </div>
+         <div class="header__logo logo" style="padding-left: 0px;"><a href="#"><h3>Mogi</h3></a></div>
+        </div>
+        </div> 
+
+        <div class="cover parallax" style="height:90%; background-image: url("https://source.unsplash.com/daily");">
+        <div class="cover__filter"></div>
+        <div class="cover__caption">
+        <div class="cover__caption__copy white-text"> 
+            <h1>'. $name .'</h1>
+            <h3>'. $bname .'</h3> <br>
+        </div>
+        </div>
+        </div> 
+        <div class="page boxed rounded">
+        <div class="page__content">
+        <h1>What is this?</h1>
+        <p>This is an official Mogi page that certificates the officiality of the badge that you have seen on the previous page <br>
+        This badge was deployed on '. $time .'</p>
+        </div>
+        </div>
+
+        <div class="footer">
+        <div class="footer__content boxed clearfix">
+            <div class="footer__content__left">
+                <p>Designed by <a href="https://twitter.com/mooonmatt"><i class="fas fa-moon"></i>moonmatt</a></p>
+            </div>
+            <div class="footer__content__right">
+                <a href="https://twitter.com/mooonmatt"><ul><i class="fab fa-twitter"></i></ul></a>
+                <a href="https://github.com/moonmatt/Mogi"><ul><i class="fab fa-github"></i></ul></a>
+                <a href="https://moonmatt.dev/"><ul><i class="fab fa-chrome"></i></ul></a>
+            </div>
+        </div>
+        </div>
+        </body>
+        </html>
+        ';
+        if (file_exists($fname)) {
+            echo "The file $fname exists";
+        } else {
+            $handle = fopen($fname, 'w') or die('Cannot open file:  '.$fname); //implicitly creates file
+            $a = ++$badgenum;
+            file_put_contents($file, $a);
+            fclose($myfile);
+            mail($rec,$sub,$msg,$headers);
+            fwrite($handle, $data);
+            header('location: yay.php');
+        }
         } else {
         header('location: sorry.php');
         }
@@ -89,10 +153,6 @@ function responsive() {
            <img src="../img/logo.png" alt="" width="40px" height="40px;">
        </div>
         <div class="header__logo logo" style="padding-left: 0px;"><a href="#"><h3>Mogi</h3></a></div>
-        <div class="header__menu" id="header__menu" >
-           <a href="badges.html"><div class="header__menu__item"><ul>Badges</ul></div></a>
-        </div>
-    <a href="#"><div class="header__menu__item responsiveicon" onclick="responsive()"><ul><i class="fas fa-bars"></i></ul></div></a>
     </div>
     </div>
     
@@ -101,14 +161,19 @@ function responsive() {
 		<div class="cover__filter"></div>
 		<div class="cover__caption">
 			<div class="cover__caption__copy white-text">
-                <h1><?php echo $bname;?> badge is now out!</h1>
-                <?php 
-                if ($ab == 1){
+            <?php
+            if($ok != "yes"){
+                echo "<h1>There are no badges at the moment</h1>";
+                echo "The next badge will be released on ". $btime;
+            } else {
+                echo "<h1>" . $bname . " badge is now out!</h1>";
+                    if ($ab == 1){
                     echo "There is only 1 badge left";
-                } else {
+                    } else {
                     echo "There are ". $ab . " badges left";
-                }
-                ?>
+                    }
+            }
+            ?>
 			</div>
 		</div>
     </div>
@@ -147,10 +212,40 @@ function responsive() {
        </div>
    </div>
 
+   <div class="page boxed rounded shadow">
+        <div class="page__content">
+        <h1>What is this?</h1>
+        <p>This is a page where you can try to get exclusive badges that you can insert in the footer of your websites made with Mogi.</p>
+        </div>
+    </div>
+
+    <div class="page boxed rounded shadow">
+        <div class="page__content">
+        <h1>All the badges</h1>
+        <p>Here you'll find all the badges released on Mogi</p>
+        <hr>
+        <div class="grid clearfix">
+            <div class="grid-3">Title</div>
+            <div class="grid-3">Number of badges</div>
+            <div class="grid-3">Badge</div>
+        </div>
+        <div class="grid clearfix">
+            <div class="grid-3">Contributor</div>
+            <div class="grid-3">1</div>
+            <div class="grid-3"><center><img src="https://i.ibb.co/tCCvZDL/Untitled-design-1.jpg" alt="Contributor" style="float: center;" width="25px" height="15px"></center></div>
+        </div>
+        <div class="grid clearfix">
+            <div class="grid-3">Early Supporter</div>
+            <div class="grid-3">5</div>
+            <div class="grid-3"><center><img src="https://i.ibb.co/545y0vF/icon.jpg" alt="Early Supporter" style="float: center;" width="25px" height="15px"></center></div>
+        </div>
+        </div>
+    </div>
+
               <div class="footer">
         <div class="footer__content boxed clearfix">
         <div class="footer__content__left">
-            <p>Designed by <a href="https://twitter.com/mooonmatt"><i class="fas fa-moon"></i>moonmatt</a></p><a href='https://mogi.moonmatt.dev/u/matteo'><img src='https://i.ibb.co/545y0vF/icon.jpg' class='badge' title='Early Supporter'></a>
+            <p>Designed by <a href="https://twitter.com/mooonmatt"><i class="fas fa-moon"></i>moonmatt</a></p><a href='https://moonmatt.dev'><img src='https://i.ibb.co/tCCvZDL/Untitled-design-1.jpg' class='badge' title='Contributor'></a>
         </div>
                 <div class="footer__content__right">
             <a href="https://twitter.com/mooonmatt"><ul><i class="fab fa-twitter"></i></ul></a>
---
+++ Website/u/moonmatt.php
@@ -0,0 +1,55 @@
+
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <title>Mogi Framework - fast and free</title>
+        <link id="pagestyle" rel="stylesheet" type="text/css" href="..\css\mogi.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
+        <script src="../js/style.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <title>moonmatt - Mogi Badges</title>
+        </head>
+        <body>
+        <div class="header clearfix sticky">
+        <div class="header__content boxed">
+                <div class="header__logo__img" style="margin: 10px;">
+            <img src="../img/logo.png" alt="" width="40px" height="40px;">
+        </div>
+         <div class="header__logo logo" style="padding-left: 0px;"><a href="#"><h3>Mogi</h3></a></div>
+        </div>
+        </div> 
+
+        <div class="cover parallax" style="height:30%; background-image: url("https://source.unsplash.com/daily");">
+        <div class="cover__filter"></div>
+        <div class="cover__caption">
+        <div class="cover__caption__copy white-text"> 
+            <h1>moonmatt</h1>
+            <h3>Early Supporter</h3> <br>
+        </div>
+        </div>
+        </div> 
+        <div class="page boxed rounded">
+        <div class="page__content">
+        <h1>What is this?</h1>
+        <p>This is an official Mogi page that certificates the officiality of the badge that you have seen on the previous page <br>
+        This badge was deployed on <code>11/08/2019</code></p>
+        </div>
+        </div>
+
+        <div class="footer">
+        <div class="footer__content boxed clearfix">
+            <div class="footer__content__left">
+                <p>Designed by <a href="https://twitter.com/mooonmatt"><i class="fas fa-moon"></i>moonmatt</a></p>
+            </div>
+            <div class="footer__content__right">
+                <a href="https://twitter.com/mooonmatt"><ul><i class="fab fa-twitter"></i></ul></a>
+                <a href="https://github.com/moonmatt/Mogi"><ul><i class="fab fa-github"></i></ul></a>
+                <a href="https://moonmatt.dev/"><ul><i class="fab fa-chrome"></i></ul></a>
+            </div>
+        </div>
+        </div>
+        </body>
+        </html>
+        
